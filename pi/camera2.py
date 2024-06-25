f82af101a49c0f71980287be05b8a2f1643e2d2e
minjunj <minjun_jo@gm.gist.ac.kr>
2024-06-25 20:45:45 +0900

    create : firsh commit

--- pi/camera2.py
+++ pi/camera2.py
@@ -2,4 +2,4 @@ from picamera2 import Picamera2
 picam2 = Picamera2()
 
 while True:
-    picam2.start_and_capture_file("test.jpg", delay=0.5, show_preview=False)
+    picam2.start_and_capture_file("test.jpg", initial_delay=0, delay=0.1, show_preview=False)
